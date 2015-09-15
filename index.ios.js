5538bcb97cc54c185cf65b554ab546f68c9ceacf
Josh Habdas <jhabdas@gmail.com>
2015-09-15 14:13:47 -0500

    Removing cruft

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,4 @@
 let React = require('react-native');
 let App = require('./src/components/App')
-let IconNavigator = require('./src/components/IconNavigator');
 
 React.AppRegistry.registerComponent('SmartphoneSymphony', () => App);
